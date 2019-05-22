592fc4f2f6ff015c55b0f1e4859d5ec63340c9da
AndreaJegher <andreaj-93@hotmail.it>
2019-05-22 19:49:52 +0200

    Tokenizer algorithm implemented

--- pkg/moltp/moltp.go
+++ pkg/moltp/moltp.go
@@ -1,5 +1,11 @@
 package moltp
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type (
 	// RawFormula object holding a single unparsed formula encoded using a TEX notation
 	RawFormula struct {
@@ -21,6 +27,19 @@ type (
 		Right []formula
 	}
 
+	token struct {
+		Value string // token symbol value
+		IsTe  bool   // is terminal
+		IsLB  bool   // is left braket
+		IsRB  bool   // is right braket
+		IsOp  bool   // is operator
+		UnOp  bool   // is unary operator
+		BiOp  bool   // is binary oprator
+		MuOp  bool   // is miltiple arguments operator
+		IsCo  bool   // is comma for multiple args operators
+		Skip  int    // how many char was have to be skipped from input
+	}
+
 	operator interface {
 	}
 
@@ -35,29 +54,211 @@ type (
 )
 
 const (
-	// symbolNOT     = "\\lnot"
-	// symbolOR      = "\\lor"
-	// symbolAND     = "\\land"
-	symbolIMPLIES = "\\to"
-	symbolALL     = "\\forall"
-	symbolEXISTS  = "\\exists"
-	symbolBOX     = "\\Box"
-	symbolDIAMOND = "\\Diamond"
+	sBOX     = "Box"
+	sDIAMOND = "Diamond"
+	sEXISTS  = "Exists"
+	sFORALL  = "Forall"
+	sIFF     = "Iff"
+	sIMPLIES = "Implies"
+	sAND     = "And"
+	sOR      = "Or"
+	sNOT     = "Not"
 )
 
-var (
-	terminals = []string{}
-)
+func operatorPreceeds(a, b *token) bool {
+	if a.MuOp {
+		return false
+	}
+	if a.UnOp {
+		return true
+	}
+	switch a.Value {
+	case sOR:
+		switch b.Value {
+		case sOR:
+			return false
+		case sAND:
+			return true
+		case sIMPLIES:
+			return true
+		case sIFF:
+			return true
+		}
+	case sAND:
+		switch b.Value {
+		case sOR:
+			return false
+		case sAND:
+			return false
+		case sIMPLIES:
+			return true
+		case sIFF:
+			return true
+		}
+	case sIMPLIES:
+		switch b.Value {
+		case sOR:
+			return false
+		case sAND:
+			return false
+		case sIMPLIES:
+			return false
+		case sIFF:
+			return true
+		}
+	case sIFF:
+		return false
+	}
+	return false
+}
 
-func parseRawFormula(rf RawFormula) (formula, error) {
-	return formula{}, nil
+// the len(string) was left here instead of the immediate value to better understand from where the value came
+func matchOperator(o, t byte) *token {
+	switch o {
+	case 'B':
+		return &token{IsOp: true, UnOp: true, Value: sBOX, Skip: len("\\Box")}
+	case 'D':
+		return &token{IsOp: true, UnOp: true, Value: sDIAMOND, Skip: len("\\Diamond")}
+	case 'e':
+		return &token{IsOp: true, MuOp: true, Value: sEXISTS, Skip: len("\\exists")}
+	case 'f':
+		return &token{IsOp: true, MuOp: true, Value: sFORALL, Skip: len("\\forall")}
+	case 'i':
+		return &token{IsOp: true, BiOp: true, Value: sIFF, Skip: len("\\iff")}
+	case 't':
+		return &token{IsOp: true, BiOp: true, Value: sIMPLIES, Skip: len("\\to")}
+	}
+	switch t {
+	case 'a':
+		return &token{IsOp: true, BiOp: true, Value: sAND, Skip: len("\\land")}
+	case 'o':
+		return &token{IsOp: true, BiOp: true, Value: sOR, Skip: len("\\lor")}
+	case 'n':
+		return &token{IsOp: true, UnOp: true, Value: sNOT, Skip: len("\\lnot")}
+	}
+	return nil
+}
+
+func nextToken(s string) *token {
+	switch s[0] {
+	case '(':
+		return &token{IsLB: true, Value: "Round", Skip: 1}
+	case '[':
+		return &token{IsLB: true, Value: "Square", Skip: 1}
+	case '{':
+		return &token{IsLB: true, Value: "Curly", Skip: 1}
+	case ')':
+		return &token{IsRB: true, Value: "Round", Skip: 1}
+	case ']':
+		return &token{IsRB: true, Value: "Square", Skip: 1}
+	case '}':
+		return &token{IsRB: true, Value: "Curly", Skip: 1}
+	case '\\':
+		return matchOperator(s[1], s[2])
+	default:
+		return &token{IsTe: true, Value: fmt.Sprintf("%c", s[0]), Skip: 1}
+	}
+}
+
+// Based on Shunting Yard Algorithm
+// 1.  While there are tokens to be read:
+// 2.        Read a token
+// 3.        If it's a terminal add it to the token queue
+// 4.        If it's an operator
+// 5.               While there's an operator on the top of the operator stack with greater precedence:
+// 6.                       Pop operators from the operator stack onto the token queue
+// 7.               Push the current operator onto the operator stack
+// 8.        If it's a left bracket push it onto the operators stack
+// 9.        If it's a right bracket
+// 10.            While there's not a left bracket at the top of the stack:
+// 11.                     Pop operators from the stack onto the output queue.
+// 12.             Pop the left bracket from the stack and discard it
+// 13. While there are operators on the stack, pop them to the queue
+func tokenize(s string, term byte) ([]*token, error) {
+	var tokens []*token
+	var ops []*token
+	var offset int
+	segment := s
+	t := nextToken(s)
+	for t != nil {
+		fmt.Println("Parsing token", t)
+		if t.IsTe {
+			tokens = append(tokens, t)
+		}
+		if t.IsOp {
+			for len(ops) > 0 {
+				k := ops[len(ops)-1]
+				if k.IsOp && operatorPreceeds(k, t) {
+					ops = ops[:len(ops)-1]
+					tokens = append(tokens, k)
+					fmt.Printf("%s preceeds %s\n", k.Value, t.Value)
+				} else {
+					fmt.Printf("%s does not preceeds %s\n", k.Value, t.Value)
+					break
+				}
+			}
+			ops = append(ops, t)
+		}
+		if t.IsLB {
+			ops = append(ops, t)
+		}
+		if t.IsRB {
+			for len(ops) > 0 {
+				k := ops[len(ops)-1]
+				if k.IsLB && k.Value == t.Value {
+					ops = ops[:len(ops)-1]
+					break
+				} else {
+					ops = ops[:len(ops)-1]
+					tokens = append(tokens, k)
+				}
+				if len(ops) == 0 {
+					return tokens, errors.New("missing opening brakets")
+				}
+			}
+		}
+		fmt.Println("Tokens")
+		for i := len(tokens) - 1; i >= 0; i-- {
+			fmt.Printf("%d: %s\n", len(tokens)-i, tokens[i].Value)
+		}
+		fmt.Println("OPs")
+		for i := len(ops) - 1; i >= 0; i-- {
+			fmt.Printf("%d: %s\n", len(ops)-i, ops[i].Value)
+		}
+		offset = offset + t.Skip
+		segment := segment[offset:]
+		if len(segment) < 1 {
+			break
+		} else {
+			t = nextToken(segment)
+		}
+	}
+	for i := len(ops) - 1; i >= 0; i-- {
+		tokens = append(tokens, ops[i])
+		// ops = ops[:len(ops)-1]
+	}
+	return tokens, nil
+}
+
+func parseRawFormula(rf RawFormula) (*formula, error) {
+	top := &formula{}
+	tokens, err := tokenize(strings.Replace(rf.Formula, " ", "", -1), 0x00)
+	if err != nil {
+		return top, err
+	}
+	fmt.Println("Final")
+	for i := len(tokens) - 1; i >= 0; i-- {
+		fmt.Printf("%d: %s\n", len(tokens)-i, tokens[i].Value)
+	}
+	// generateFormulaTree
+	return top, err
 }
 
 func encodeSequent(s sequent) (RawSequent, error) {
 	return RawSequent{}, nil
 }
 
-func proveFormula(f formula) ([]sequent, error) {
+func proveFormula(f *formula) ([]sequent, error) {
 	return []sequent{}, nil
 }
 
