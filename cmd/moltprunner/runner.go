a2a7d3213024da8c77563d66ffd766483a27243a
AndreaJegher <andreaj-93@hotmail.it>
2019-05-25 16:43:29 +0200

    Added console command and file renaming

---
+++ cmd/moltprunner/runner.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/gomoltp/pkg/moltp"
+)
+
+var (
+	debugOn bool
+	formula string
+)
+
+func init() {
+	flag.StringVar(&formula, "f", "\\Box ( a \\to b ) \\to  ( \\Box a \\to \\Box b )", "Formula to be solved.")
+	flag.BoolVar(&debugOn, "v", false, "Swith for log printing")
+}
+
+func main() {
+	flag.Parse()
+	var rf moltp.RawFormula
+	rf.OID = 0
+	rf.Formula = formula
+	solution, err := moltp.Prove(rf, debugOn)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(solution)
+	}
+}
