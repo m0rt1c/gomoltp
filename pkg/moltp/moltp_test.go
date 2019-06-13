447d65f4548e4e0bd6a21ce45cc7fb2be6534caa
AndreaJegher <andreaj-93@hotmail.it>
2019-06-13 23:56:16 +0200

    Added new test

--- pkg/moltp/moltp_test.go
+++ pkg/moltp/moltp_test.go
@@ -34,3 +34,27 @@ func TestReduceANDFormula(t *testing.T) {
 		t.Errorf("got %s want %s", fmt.Sprint(out), fmt.Sprint(g3))
 	}
 }
+
+func TestProver(t *testing.T) {
+	rf := &RawFormula{OID: 0, Formula: "\\Box a \\to \\Box \\Box a"}
+	prover := Prover{Debug: false}
+	solution, err := prover.Prove(rf)
+	if err != nil {
+		t.Errorf("got error %s want nil", err)
+	} else {
+		out := []string{
+			"S1:  <- |( ( Box a ) Implies ( Box ( Box a ) ) )|_{0} []",
+			"S3: |( Box a )|_{0} <-  [R4 S1]",
+			"S2:  <- |( Box ( Box a ) )|_{0} [R3 S1]",
+			"S5:  <- |( Box a )|_{1:0} [R7 S2]",
+			"S7:  <-  [R1 S4 S6 {W0/2}]",
+		}
+		for i, o := range out {
+			s := fmt.Sprintf("%s", solution[i])
+			if o != s {
+				t.Errorf("got %s want %s", s, o)
+			}
+		}
+
+	}
+}
