c919906f206a2141277036b6190fabdba7220f2f
AndreaJegher <andreaj-93@hotmail.it>
2019-06-14 09:49:18 +0200

    Added new formula test

--- pkg/moltp/moltp_test.go
+++ pkg/moltp/moltp_test.go
@@ -35,7 +35,7 @@ func TestReduceANDFormula(t *testing.T) {
 	}
 }
 
-func TestProver(t *testing.T) {
+func TestProver1(t *testing.T) {
 	rf := &RawFormula{OID: 0, Formula: "\\Box a \\to \\Box \\Box a"}
 	prover := Prover{Debug: false}
 	solution, err := prover.Prove(rf)
@@ -50,6 +50,37 @@ func TestProver(t *testing.T) {
 			"S4: |a|_{W0:0} <-  [R8 S3]",
 			"S6:  <- |a|_{2:1:0} [R7 S5]",
 			"S7:  <-  [R1 S4 S6 {W0/2}]",
+		}
+		for i, o := range out {
+			s := fmt.Sprintf("%s", solution[i])
+			if o != s {
+				t.Errorf("got %s want %s", s, o)
+			}
+		}
+
+	}
+}
+
+func TestProver2(t *testing.T) {
+	rf := &RawFormula{OID: 0, Formula: "\\Box \\Box a \\to \\Diamond \\Diamond a"}
+	prover := Prover{Debug: false}
+	solution, err := prover.Prove(rf)
+	if err != nil {
+		t.Errorf("got error %s want nil", err)
+	} else {
+		out := []string{
+			"S1:  <- |( ( Box ( Box a ) ) Implies ( Not ( Box ( Not ( Not ( Box ( Not a ) ) ) ) ) ) )|_{0} []",
+			"S3: |( Box ( Box a ) )|_{0} <-  [R4 S1]",
+			"S4: |( Box a )|_{W0:0} <-  [R8 S3]",
+			"S2:  <- |( Not ( Box ( Not ( Not ( Box ( Not a ) ) ) ) ) )|_{0} [R3 S1]",
+			"S6: |( Box ( Not ( Not ( Box ( Not a ) ) ) ) )|_{0} <-  [R6 S2]",
+			"S7: |( Box ( Not a ) )|_{W0:0} <-  [R8 S6]",
+			"S8:  <- |( Not ( Box ( Not a ) ) )|_{W0:0} [R5 S7]",
+			"S9: |( Box ( Not a ) )|_{W0:0} <-  [R6 S8]",
+			"S10: |( Not a )|_{W1:W0:0} <-  [R8 S9]",
+			"S5: |a|_{W1:W0:0} <-  [R8 S4]",
+			"S11:  <- |a|_{W1:W0:0} [R5 S10]",
+			"S12:  <-  [R1 S5 S11 {W1/W1}]",
 		}
 		for i, o := range out {
 			s := fmt.Sprintf("%s", solution[i])
