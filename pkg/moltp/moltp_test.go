70326a9d5d36afb789dc5c9b73d0acc93ad55191
AndreaJegher <andreaj-93@hotmail.it>
2019-06-15 20:50:45 +0200

    New test

--- pkg/moltp/moltp_test.go
+++ pkg/moltp/moltp_test.go
@@ -91,3 +91,34 @@ func TestProver2(t *testing.T) {
 
 	}
 }
+
+func TestProver3(t *testing.T) {
+	rf := &RawFormula{OID: 0, Formula: "\\Diamond \\Box a \\to \\Box \\Diamond a"}
+	prover := Prover{Debug: false}
+	solution, err := prover.Prove(rf)
+	if err != nil {
+		t.Errorf("got error %s want nil", err)
+	} else {
+		out := []string{
+			"S1:  <- |( ( Not ( Box ( Not ( Box a ) ) ) ) Implies ( Box ( Not ( Box ( Not a ) ) ) ) )|_{0} []",
+			"S3: |( Box a )|_{1:0} <-  [R4 S1]",
+			"S4:  <- |( Box ( Not ( Box a ) ) )|_{0} [R5 S3]",
+			"S5:  <- |( Not ( Box a ) )|_{1:0} [R7 S4]",
+			"S6: |( Box a )|_{1:0} <-  [R6 S5]",
+			"S2:  <- |( Box ( Not ( Box ( Not a ) ) ) )|_{0} [R3 S1]",
+			"S8:  <- |( Not ( Box ( Not a ) ) )|_{2:0} [R7 S2]",
+			"S9: |( Box ( Not a ) )|_{2:0} <-  [R6 S8]",
+			"S10: |( Not a )|_{V0:2:0} <-  [R8 S9]",
+			"S7: |a|_{W0:1:0} <-  [R8 S6]",
+			"S11:  <- |a|_{V0:2:0} [R5 S10]",
+			"S12:  <-  [R1 S7 S11 {W0/V0}]",
+		}
+		for i, o := range out {
+			s := fmt.Sprintf("%s", solution[i])
+			if o != s {
+				t.Errorf("got %s want %s", s, o)
+			}
+		}
+
+	}
+}
