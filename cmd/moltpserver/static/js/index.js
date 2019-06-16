749924840d5dd71a9efee7cc4abb97f6ffc80eee
AndreaJegher <andreaj-93@hotmail.it>
2019-06-16 16:17:57 +0200

    Webpage changes

--- cmd/moltpserver/static/js/index.js
+++ cmd/moltpserver/static/js/index.js
@@ -9,9 +9,43 @@ function render(formula, renderer){
   }
 }
 
+function fillSolution(data) {
+  var k = 0
+  var s = data[k]
+  while( s != undefined ){
+    li = document.createElement('li')
+
+    solution.appendChild(li)
+    d1 = document.createElement('div')
+    d1.classList.add("sequntdivider")
+    li.appendChild(d1)
+    d2 = document.createElement('div')
+    d2.classList.add("sequntsegment")
+    li.appendChild(d2)
+    d3 = document.createElement('div')
+    d3.classList.add("sequntdivider")
+    li.appendChild(d3)
+    d4 = document.createElement('div')
+    d4.classList.add("sequntsegment")
+    li.appendChild(d4)
+    d5 = document.createElement('div')
+    d5.classList.add("sequntdivider")
+    li.appendChild(d5)
+
+    d1.innerText = String(`${s["name"]}`)
+    katex.render(String(`${s["left"]}`), d2);
+    katex.render("\\leftarrow", d3);
+    katex.render(String(`${s["right"]}`), d4);
+    d5.innerText = String(`${s["just"]}`)
+    k++
+    s = data[k]
+  }
+}
+
 function prove(){
   var data = {'oid':0, 'formula':document.querySelector("#f1").value}
   solution.innerHTML = ''
+  document.querySelector('#soltitle').innerText = "Solution"
 
   return fetch("/prover", {
     method: "POST",
@@ -24,40 +58,19 @@ function prove(){
     if (response.status != 200) {
       response.json().then(function(data){
         alert("Info: "+String(data["info"]))
+        if (response.status == 500) {
+          if (data != null && data != "null")  {
+            document.querySelector('#soltitle').innerText = "Partial result"
+            fillSolution(data["result"])
+          }
+        }
       })
     } else {
       response.json().then(function(data){
         if (data == null || data == "null")  {
           alert("Empty reponse!")
         } else {
-          var k = 0
-          var s = data[k]
-          while( s != undefined ){
-            li = document.createElement('li')
-            solution.appendChild(li)
-            d1 = document.createElement('div')
-            d1.classList.add("sequntsegment")
-            d2 = document.createElement('div')
-            d2.classList.add("sequntdivider")
-            d3 = document.createElement('div')
-            d3.classList.add("sequntsegment")
-            d4 = document.createElement('div')
-            d4.classList.add("sequntsegment")
-            d5 = document.createElement('div')
-            d5.classList.add("sequntsegment")
-            li.appendChild(d5)
-            li.appendChild(d1)
-            li.appendChild(d2)
-            li.appendChild(d3)
-            li.appendChild(d4)
-            d5.innerText = String(`${s["name"]}`)
-            katex.render(String(`${s["left"]}`), d1);
-            katex.render("\\leftarrow", d2);
-            katex.render(String(`${s["right"]}`), d3);
-            d4.innerText = String(`${s["just"]}`)
-            k++
-            s = data[k]
-          }
+          fillSolution(data)
         }
       })
     }
